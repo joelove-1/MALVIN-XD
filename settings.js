ab335deb23c1f547429057dc07a7614b6b8a540f
joelove-1 <realjoelove1@gmail.com>
2025-05-23 16:31:17 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-VARIABLES: process.env.VARIABLES || "true",    
+    
 SESSION_ID: process.env.SESSION_ID || "malvin~dqUW3ZCT#mrJjG5xKItmdveR3gVcePLnkx4YC8ZdWLBWrf4jlajI",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
@@ -18,7 +18,8 @@ MODE: process.env.MODE || "public",
 
         AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
-
+VARIABLES: process.env.VARIABLES || "true",   
+    
         AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 
